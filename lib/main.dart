d4a4024e09c394b5dc9486aaea3c20a689c33156
Guilherme Torres <36198151+Tworres@users.noreply.github.com>
2024-10-24 15:20:24 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:super_lista/providers/auth.dart';
